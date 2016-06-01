a43994c037a2e3560434318f569c8119e776df96
howardl01@live.ca <howardl01@live.ca>
2016-06-01 15:51:48 -0400

    completed list

--- exercise_6.rb
+++ exercise_6.rb
@@ -6,3 +6,27 @@ def print_grocery(grocery_list)
 end
 
 print_grocery(grocery_list)
+
+grocery_list.push ("rice")
+
+print_grocery(grocery_list)
+
+puts grocery_list.count
+
+grocery_list.include?("banana")
+
+if grocery_list.include?("banana")
+  puts "You need to pick up some bananas"
+else
+  puts "You don't need to pick up any bananas today!"
+end
+
+puts grocery_list[1]
+
+puts grocery_list.sort
+
+print_grocery(grocery_list)
+
+puts grocery_list .pop
+
+print_grocery(grocery_list)
