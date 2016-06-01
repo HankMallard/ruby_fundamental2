333a1b11365bcfa566331af14f81c6fb85734a7c
howardl01@live.ca <howardl01@live.ca>
2016-06-01 15:05:17 -0400

    listing

--- exercise_6.rb
+++ exercise_6.rb
@@ -1,1 +1,8 @@
-puts grocery_list=["jam", "cheese", "bread", "beef", "beer"
+grocery_list = ["jam", "cheese", "bread", "beef", "beer"]
+def print_grocery(grocery_list)
+   grocery_list.each do |grocery|
+     puts grocery
+   end
+end
+
+print_grocery(grocery_list)
