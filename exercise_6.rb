3410387bf75a8ee5f955f3af206eac20ea7ef9cd
howardl01@live.ca <howardl01@live.ca>
2016-06-01 15:09:57 -0400

    adding asterisk

--- exercise_6.rb
+++ exercise_6.rb
@@ -1,7 +1,7 @@
 grocery_list = ["jam", "cheese", "bread", "beef", "beer"]
 def print_grocery(grocery_list)
    grocery_list.each do |grocery|
-     puts grocery
+     puts "*#{grocery}"
    end
 end
 
