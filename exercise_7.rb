b9bf2eb70ffed0df6904a8d15692a3b819a5b052
howardl01@live.ca <howardl01@live.ca>
2016-06-01 17:10:02 -0400

    display list

--- exercise_7.rb
+++ exercise_7.rb
@@ -1,5 +1,15 @@
-puts students= {
-  :cohorts1 =>34,
-  :cohorts2 =>42,
-  :cohorts3 =>22,
+students= {
+  :cohorts1=> 34,
+  :cohorts2=> 42,
+  :cohorts3=> 22,
 }
+
+def cohort_list(students)
+  students.each do |student_key, student_value|
+    puts "#{student_key}:#{student_value} students"
+  end
+end
+
+cohort_list(students)
+
+puts students[:cohorts4]="43 students"
