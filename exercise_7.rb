d4763b995fa4dadb66987d9115e11dd4d20e2054
howardl01@live.ca <howardl01@live.ca>
2016-06-02 13:30:48 -0400

    Display with Keys

--- exercise_7.rb
+++ exercise_7.rb
@@ -3,13 +3,20 @@ students= {
   :cohorts2=> 42,
   :cohorts3=> 22,
 }
-
-def cohort_list(students)
+# Display students list with asterisks
   students.each do |student_key, student_value|
-    puts "#{student_key}:#{student_value} students"
+    puts "*#{student_key}:#{student_value} students"
   end
+
+#Adding a new cohorts
+students[:cohorts4] = 43
+puts students
+
+#Class Expansion
+students.each do |student_key, student_value|
+  sum= student_value * 1.05
+  puts "#{student_key} has increased by 5% and they now have #{student_value} students!"
 end
 
-cohort_list(students)
-
-puts students[:cohorts4]="43 students"
+#Display cohorts with keys
+puts students.keys
